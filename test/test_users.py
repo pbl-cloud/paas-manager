e4d3e6d18d76f0aec85364c519d6583fbce32bbe
ina_yuzen <leonhard.mch.chatte@gmail.com>
2014-06-23 15:13:43 +0900

    Fix tests

--- test/test_users.py
+++ test/test_users.py
@@ -1,25 +1,27 @@
 import unittest
-from paas_manager.app import users
+from paas_manager.app.users import Users
 
 
 class TestUsers(unittest.TestCase):
+    users = Users()
+
     def setUp(self):
-        users.register_user('test@test', 'test')
+        self.users.register_user('test@test', 'test')
 
     def tearDown(self):
-        users.delete_user('test@test')
+        self.users.delete_user('test@test')
 
     def test_is_registered(self):
-        self.assertTrue((users.is_registered('test@test')))
+        self.assertTrue((self.users.is_registered('test@test')))
 
     def test_verify_success(self):
-        self.assertTrue(users.verify_password('test@test', 'test'))
+        self.assertTrue(self.users.verify_password('test@test', 'test'))
 
     def test_verify_failed(self):
-        self.assertFalse(users.verify_password('test@test', 'pass'))
+        self.assertFalse(self.users.verify_password('test@test', 'pass'))
 
     def test_user_id(self):
-        id = users.user_id('test@test')
+        id = self.users.user_id('test@test')
 
 if __name__ == '__main__':
     unittest.main()
