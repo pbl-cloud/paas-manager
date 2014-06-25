6cea549afc7cdcb64d041a2c7737a35d63eca1e8
tuvistavie <tuvistavie@gmail.com>
2014-06-25 14:52:33 +0900

    Fix connection reset bug when redirect during file upload.

--- paas_manager/app/__init__.py
+++ paas_manager/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from os import mkdir
 from os.path import expanduser, exists
-from ..settings import config
 
+from ..settings import config
+from .util import StreamConsumingMiddleware
 
 UPLOAD_FOLDER = expanduser(config['app']['upload_folder'])
 
@@ -12,6 +13,8 @@ if not exists(UPLOAD_FOLDER):
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = config['security']['secret']
+app.wsgi_app = StreamConsumingMiddleware(app.wsgi_app)
+
 
 from . import context_processors
 from . import views
